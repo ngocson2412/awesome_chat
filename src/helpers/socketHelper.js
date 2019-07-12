c9bc71e61be73d65cd56fde1c140d9f668def5e4
NgocSon2412 <sonnguyenk57@gmail.com>
2019-07-12 23:55:21 +0900

    add contact realtime

---
+++ src/helpers/socketHelper.js
@@ -0,0 +1,25 @@
+export let pushSocketIdtoArray = (clients, userId, socketId) => {
+    if(clients[userId]) {
+        clients[userId].push(socketId)
+    }else {
+        clients[userId] = [socketId]
+    }
+    return clients
+}
+
+export let emitNotifyArray = (clients, userId, io, eventName, data) => {
+    clients[userId].forEach(socketId => {
+        return io.sockets.connected[socketId].emit(eventName, data)
+    })
+}
+
+export let removeSoketIdFromArray = (clients, userId, socket) => {
+    clients[userId] = clients[userId].filter((socketId) =>{
+        return socketId !== socket.id
+    })
+
+    if (!clients[userId].length) {
+        delete clients[userId]
+    }
+    return clients
+}
